1ae0162751054561db27d8fe5c81fece3f9c4508
Dell <Dell@DESKTOP-6P8TEK1>
2022-04-12 14:49:42 +0530

    first commit

---
+++ Day2_GitHub/src/com/intro/git/Intro1.java
@@ -0,0 +1,19 @@
+package com.intro.git;
+
+public class Intro1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		float f=13.5f;
+		double d=14.5f;
+		char g='a';
+		String str="Akshay";		
+		System.out.println(a);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(g);
+		System.out.println(str);
+
+	}
+
+}

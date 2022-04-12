bffb535ff682fc6701b7f3b3aac8c7907ec2c1df
Dell <Dell@DESKTOP-6P8TEK1>
2022-04-12 15:19:05 +0530

    first commit

---
+++ Day2_GitHub/src/com/intro/git/Intro2.java
@@ -0,0 +1,17 @@
+package com.intro.git;
+
+public class Intro2 {
+	
+	String str="Akshay";
+	void print() {
+		System.out.println(str);
+	}
+	
+
+	public static void main(String[] args) {
+	Intro2 car=new Intro2();
+	car.print();
+
+	}
+
+}

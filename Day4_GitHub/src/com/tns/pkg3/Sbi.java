f954f11293d2b2f6f9070dc39be45ba599f5c2ba
Dell <Dell@DESKTOP-6P8TEK1>
2022-04-13 13:55:08 +0530

    first commit

---
+++ Day4_GitHub/src/com/tns/pkg3/Sbi.java
@@ -0,0 +1,15 @@
+package com.tns.pkg3;
+
+public class Sbi {
+
+	private int balance;
+	
+	public int getbalance() {
+		return balance;
+	}
+	public void setbalance(int bal)
+	{
+		balance=bal;
+	}
+
+}
---
+++ Day4_GitHub/src/com/tns/pkg4/Encapsulation.java
@@ -0,0 +1,14 @@
+package com.tns.pkg4;
+
+import com.tns.pkg3.Sbi;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		Sbi s=new Sbi();
+		s.setbalance(32000);
+		System.out.println(s.getbalance());
+
+	}
+
+}

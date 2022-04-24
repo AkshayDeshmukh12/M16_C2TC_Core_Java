f622a2838c73881e5269afb02fd8a07915143941
Dell <Dell@DESKTOP-6P8TEK1>
2022-04-24 12:51:09 +0530

    first commit

---
+++ Array/src/com/m16/Array2.java
@@ -0,0 +1,28 @@
+package com.m16;
+
+public class Array2 {
+
+	
+		
+
+		public static void main(String[] args)
+
+		{
+
+		int[] a={10,20,30,40};
+
+		for (int i=0;i<a.length;i++)
+
+		{
+
+		System.out.println(a[i]);
+
+		}
+
+		}
+
+		
+
+	}
+
+

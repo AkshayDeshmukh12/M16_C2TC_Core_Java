ff3f067100096f12a9048745fe1ca85ac4554900
Dell <Dell@DESKTOP-6P8TEK1>
2022-04-28 10:43:44 +0530

    first commit

---
+++ Day14_String/src/com/m16/pkg/Exampl2.java
@@ -0,0 +1,31 @@
+package com.m16.pkg;
+import java.util.Scanner;
+public class Exampl2 {
+
+	public static void main(String[] args) {
+		
+
+		
+		Scanner in = new Scanner(System.in);
+
+		String str = "Dhoni";
+
+		String s = in.next();
+
+		if(str.equals(s)){
+
+		System.out.print("Matching");
+
+		}
+
+		else{
+
+		System.out.print("Not matching");
+
+		}
+
+		
+
+	}
+
+}

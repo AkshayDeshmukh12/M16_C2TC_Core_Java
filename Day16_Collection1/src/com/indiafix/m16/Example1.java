b3d9df075f004a545706a091ca69299c461b6b49
Dell <Dell@DESKTOP-6P8TEK1>
2022-05-09 13:31:16 +0530

    first commit

---
+++ Day16_Collection1/src/com/indiafix/m16/Example1.java
@@ -0,0 +1,30 @@
+package com.indiafix.m16;
+import java.util.*;
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+
+		
+
+		ArrayList arr = new ArrayList();
+
+		arr.add(3);
+
+		arr.add("Methods");
+
+		arr.add("Java");
+
+		arr.add(4);
+
+		Vector v = new Vector();
+
+		v.addAll(arr);
+
+		System.out.println(v);
+
+		
+
+	}
+
+}
---
+++ Day16_Collection1/src/com/indiafix/m16/Example2.java
@@ -0,0 +1,26 @@
+package com.indiafix.m16;
+import java.util.Vector;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		
+		
+
+		Vector al=new Vector(); //Vector declaration
+
+		al.add(65);
+
+		al.add("People");
+
+		al.add(65);
+
+		System.out.println(al);
+
+		
+
+		
+
+	}
+
+}

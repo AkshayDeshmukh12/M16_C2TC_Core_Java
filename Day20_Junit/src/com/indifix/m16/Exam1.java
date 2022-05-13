964c14cc3a1a672ecd811b7db436e231bc75831d
Dell <Dell@DESKTOP-6P8TEK1>
2022-05-13 08:26:40 +0530

    first commit

---
+++ Day20_Junit/src/com/indifix/m16/Exam1.java
@@ -0,0 +1,18 @@
+package com.indifix.m16;
+import org.junit.jupitor.api.Test;
+public class Exam1 {
+
+	public static void main(String[] args) {
+		@Test
+		void display()
+		{
+			System.out.println("Welcome to CT2C");	
+		}
+		@Test
+		void print()
+		{
+			System.out.println("Welcome To core Java");
+		}
+	
+	}
+}
---
+++ Day20_Junit/src/org/junit/jupitor/api/Test.java
@@ -0,0 +1,21 @@
+package org.junit.jupitor.api;
+import org.junit.jupitor.api.Test;
+public class Test {
+
+	public static void main(String[] args) {
+		
+			
+			void display()
+			{
+				System.out.println("Welcome to CT2C");	
+			}
+			
+			void print()
+			{
+				System.out.println("Welcome To core Java");
+			}
+		
+
+	}
+
+}
